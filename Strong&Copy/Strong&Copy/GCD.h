78c6c809840fb6f0f607e663a02e90993ff1bd2c
yanhangpu <sogou@sogoudeiMac.local>
2018-05-11 13:03:13 +0800

    commit

---
+++ Strong&Copy/Strong&Copy/GCD.h
@@ -0,0 +1,13 @@
+//
+//  GCD.h
+//  Strong&Copy
+//
+//  Created by sogou-Yan on 2018/5/8.
+//  Copyright © 2018年 sogou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCD : NSObject
+
+@end
--- Strong&Copy/Strong&Copy/Person.h
+++ Strong&Copy/Strong&Copy/Person.h
@@ -17,7 +17,7 @@
 //copy 创建新的地址空间存储目标数据
 @property(nonatomic, copy) NSString *secondName;
 
-@property(nonatomic, assign) NSString *thirdName;
+//@property(nonatomic, assign) NSString *thirdName;
 
 #warning property with retain or strong attribute must be of object type
 //@property(nonatomic, strong) int number;
