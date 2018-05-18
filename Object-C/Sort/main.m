15bfc1e4d66cb7183753a07bd82dd75eeb35a28d
yanhangpu <sogou@sogoudeiMac.local>
2018-05-18 10:12:46 +0800

    sort update

--- Object-C/Sort/main.m
+++ Object-C/Sort/main.m
@@ -33,10 +33,6 @@ void quickSort(int *a, int indexLeft,int indexRight) {
     quickSort(a, i+1, indexRight);
 }
 
-
-
-
-
 void sortES(int *a,int count) {
     for (int i = 0; i<count-1; i++) {
         for (int j = 0; j<count-i-1; j++) {
@@ -50,76 +46,12 @@ void sortES(int *a,int count) {
     }
 }
 
-typedef struct NODE {
-    struct NODE *next;
-    int number;
-} node;
-
-node * createLinkLish(int length) {
-    if (!length) {
-        return NULL;
-    }
-    node *head,*p,*q;
-    int number = 1;
-    head = (node *)malloc(sizeof(node));
-    head->number = 1;
-    head->next = head;
-    p = q = head;
-    while (++number<=length) {
-        p = (node *)malloc(sizeof(node));
-        p->number = number;
-        p->next = NULL;
-        q->next = p;
-        q = p;
-    }
-    return head;
-}
-
-void printfLinkLish(node *head) {
-    if (head==NULL) {
-        return;
-    }
-    node *p = head;
-    while (p) {
-        node *q = p->next;
-        printf("%d %d \n",p->number,q->number);
-        p = p->next;
-    }
-    printf("\n");
-}
-
-node * reverseFunc(node *head) {
-    if (head==NULL) {
-        return head;
-    }
-    node *p,*q;
-    p = head;
-    q = NULL;
-    while (p) {
-        node *pNext = p->next;
-        p->next = q;
-        q = p;
-        p = pNext;
-    }
-    return q;
-}
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-//        int a[12] = {1,9,5,8,2,7,6,10,20,56,23,32};
-//        for (int i = 0; i<12; i++) {
-//            printf(" %d ",a[i]);
-//        }
-        
-        
-        node *head = createLinkLish(7);
-        if (head) {
-            printfLinkLish(head);
-//            node *reverseHead = reverseFunc(head);
-//            printfLinkLish(reverseHead);
-//            free(reverseHead);
+        int a[12] = {1,9,5,8,2,7,6,10,20,56,23,32};
+        for (int i = 0; i<12; i++) {
+            printf(" %d ",a[i]);
         }
-        reverseFunc(nil);
     }
     return 0;
 }
