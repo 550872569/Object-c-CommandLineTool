bd832db8000305c6b97ec528f9554c92450ce336
YY <550872569@qq.com>
2018-05-29 08:32:28 +0800

    find max number

--- Object-C/Sort_0/main.m
+++ Object-C/Sort_0/main.m
@@ -8,10 +8,53 @@
 
 #import <Foundation/Foundation.h>
 
+
+void findMax(int *a, int count, int *max) {
+    if (count<=0) {
+        *max = -1;
+    }
+    int temp = a[0];
+    for (int i = 0; i<count; i++) {
+        if (a[i]>temp) {
+            temp = a[i];
+        }
+    }
+    *max = temp;
+}
+
+void find_2_max(int *a, int count, int *first, int *second) {
+    if (count<=0) {
+        *first = -1;
+        *second = -1;
+    }
+    if (a[0]>a[1]) {
+       *first = a[0];
+       *second = a[1];
+    } else if (a[1]>a[0]) {
+        *first = a[1];
+        *second = a[0];
+    }
+    for (int i = 2; i<count; i++) {
+        if (a[i]>*first) {
+            *second = *first;
+            *first = a[i];
+        } else if (a[i]>*second) {
+            *second = a[i];
+        }
+    }
+    
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int a[10] = {1,2,3,4,100,6,7,8,99,90};
+        int max = 0;
+        findMax(a, 10, &max);
+        printf("%d\n",max);
+        int first = 0;
+        int second = 0;
+        find_2_max(a, 10, &first, &second);
+        printf("first = %d \n second = %d\n",first,second);
     }
     return 0;
 }
